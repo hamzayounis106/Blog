3362f6fa33a826823e12d56c064b53e4efd4ba9b
Hamza Younis <hamzayounis106@gmail.com>
2024-07-20 20:24:14 +0500

    chore: Update CORS configuration and API endpoints for client and server deployments

--- Server/app.js
+++ Server/app.js
@@ -16,32 +16,19 @@ mongoose.connect(process.env.MONGO_URI);
 app.use(express.json({ limit: "0.5mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "500mb" }));
-app.use(function (req, res, next) {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // res.setHeader("Access-Control-Max-Age", "1800");
-  // res.setHeader("Access-Control-Allow-Headers", "content-type");
-  // res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-  // next();
-});
+
 app.use(cors());
 // app.use(
 //   cors({
 //     origin: [
-//       // "https://gupshup-client-one.vercel.app",
+//       // "https://gupshup-client-one.vercel.app", 
 //       "https://blog-client-bice.vercel.app",
 //       // "http://localhost:5173",
 //     ],
-//     credentials: true,
+//     credentials: true, 
 //   })
 // );
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://blog-client-bice.vercel.app",
-  })
-);
-
+app.use(cors({credentials: true, origin: 'https://blog-client-bice.vercel.app'}));
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
