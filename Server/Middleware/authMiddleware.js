da595d6d81254bf9bba2dc52370f60babf306e19
Hamza Younis <hamzayounis106@gmail.com>
2024-07-20 18:30:26 +0500

    chore: Add console log for decoded token in authMiddleware

--- Server/Middleware/authMiddleware.js
+++ Server/Middleware/authMiddleware.js
@@ -5,12 +5,13 @@ env.config();
 const secretKey = process.env.SECRET_KEY;
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.auth_token;
+  console.log("Token is: " + token);
   if (!token) {
     return res.status(401).send('Unauthorized');
   }
   try {
     const decoded = jwt.verify(token, secretKey);
-    console.log(decoded)
+    console.log("Decoded is " + decoded)
     req.user = decoded;
     // res.send(decoded.email);
     // console.log(decoded);
