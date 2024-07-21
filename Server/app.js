24e599f67058efcc85cbe3f589cfd1a05145d949
Hamza Younis <hamzayounis106@gmail.com>
2024-07-21 12:28:30 +0500

    chore: Update CORS configuration and API endpoints for client and server deployments

--- Server/app.js
+++ Server/app.js
@@ -8,27 +8,20 @@ import bodyParser from "body-parser";
 import authRoutes from "./Routes/authRoutes.js";
 import userRoutes from "./Routes/userRoutes.js";
 import postRoutes from "./Routes/postRoutes.js";
+
 const app = express();
 
-// mongoose.connect("");
-
 mongoose.connect(process.env.MONGO_URI);
+
 app.use(express.json({ limit: "0.5mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "500mb" }));
 
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: [
-//       // "https://gupshup-client-one.vercel.app", 
-//       "https://blog-client-bice.vercel.app",
-//       // "http://localhost:5173",
-//     ],
-//     credentials: true, 
-//   })
-// );
-app.use(cors({credentials: true, origin: 'https://blog-client-bice.vercel.app'}));
+app.use(cors({
+  origin: 'https://blog-client-bice.vercel.app',
+  credentials: true
+}));
+
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
