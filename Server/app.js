f8b320c32aeb8f3cd3e5467c6b8ddf54ff8406dd
Hamza Younis <hamzayounis106@gmail.com>
2024-07-21 12:38:56 +0500

    chore: Update CORS configuration to allow multiple origins and methods

--- Server/app.js
+++ Server/app.js
@@ -17,10 +17,12 @@ app.use(express.json({ limit: "0.5mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "500mb" }));
 
-app.use(cors({
-  origin: 'https://blog-client-bice.vercel.app',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://blog-client-bice.vercel.app", "http://localhost:5173/"],
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
