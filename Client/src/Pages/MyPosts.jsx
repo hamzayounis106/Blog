517600e4866050298f2d9648e1cc3aa0688f8db6
Hamza Younis <hamzayounis106@gmail.com>
2024-07-23 17:00:29 +0500

    chore: Refactor MyPosts component to improve loading behavior and UI

--- Client/src/Pages/MyPosts.jsx
+++ Client/src/Pages/MyPosts.jsx
@@ -36,42 +36,48 @@ function MyPosts() {
   };
 
   return (
+   <>
+   {this.state.loading  ? (
+    <p>Loading....</p>
+   ):(
     <div className="flex flex-col bg-gray-50">
-      {allowed ? (
-        <>
-          {myposts ? (
-            <>
-              <div className="flex flex-col items-center justify-center w-full py-4">
-                <h1 className="text-2xl font-bold">My Posts</h1>
-       
-              <div className="grid grid-cols-3 gap-4 p-4">
-                {myposts.map((post) => (
-                  <MyPostCard
-                    key={post._id}
-                    id={post._id}
-                    title={post.title}
-                    picture={post.imageUrl}
-                    postedBy={post.userName}
-                    authorImage={post.userProfileImage}
-                    excerpt={post.excrept}
-                    saved={post.saved}
-                    onPostRemove={handlePostRemoval}
-                    className="w-full h-full"
-                  />
-                ))}
-              </div>
-              </div>
-            </>
-          ) : (
-            <div className="flex flex-col items-center justify-center h-full">
-              <h1 className="text-2xl font-bold">Your Posts will appear here</h1>
+    {allowed ? (
+      <>
+        {myposts ? (
+          <>
+            <div className="flex flex-col items-center justify-center w-full py-4">
+              <h1 className="text-2xl font-bold">My Posts</h1>
+     
+            <div className="grid grid-cols-3 gap-4 p-4">
+              {myposts.map((post) => (
+                <MyPostCard
+                  key={post._id}
+                  id={post._id}
+                  title={post.title}
+                  picture={post.imageUrl}
+                  postedBy={post.userName}
+                  authorImage={post.userProfileImage}
+                  excerpt={post.excrept}
+                  saved={post.saved}
+                  onPostRemove={handlePostRemoval}
+                  className="w-full h-full"
+                />
+              ))}
             </div>
-          )}
-        </>
-      ) : (
-        <AccessDenied />
-      )}
-    </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-2xl font-bold">Your Posts will appear here</h1>
+          </div>
+        )}
+      </>
+    ) : (
+      <AccessDenied />
+    )}
+  </div>
+   )}
+   </>
   );
 }
 
